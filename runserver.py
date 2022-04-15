19f988186105b0101852a4c6f7fc3280b097c6d5
José Martins Camargo <josemartinscamargo@MacBook-Pro-de-Jose.local>
2022-04-15 19:15:59 -0300

    mudanca porta

--- runserver.py
+++ runserver.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
         key='ENVIRONMENT', default='production') == 'development' else False
     debug = environment.get_item(key='DEBUG', default=False)
     host = environment.get_item(key='HOST', default='0.0.0.0')
-    port = int(environment.get_item(key='PORT', default=8000))
+    port = int(environment.get_item(key='PORT', default=5000))
     uvicorn.run("application:app", host="0.0.0.0", port=port,
                 reload=reload, debug=debug)
